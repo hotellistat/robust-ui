074509dfc1c9154bef3ec3ab67fa84d4ef67ada7
Daniel Schneider <daniel.schneider@eramux.com>
2022-01-25 16:48:19 +0100

    feat: add components

---
+++ src/components/Notice/Notice.vue
@@ -0,0 +1,29 @@
+<template>
+  <div :class="computedClass" class="mb-4 border-l-4 p-4">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+import { computed, defineComponent, PropType } from 'vue'
+import variants from './variants'
+
+export default defineComponent({
+  props: {
+    variant: {
+      type: String as PropType<'info' | 'warning' | 'error' | 'success'>,
+      default: 'info',
+    },
+  },
+
+  setup(props) {
+    const computedClass = computed(() => {
+      return variants[props.variant]
+    })
+
+    return {
+      computedClass,
+    }
+  },
+})
+</script>
---
+++ src/components/Notice/variants.ts
@@ -0,0 +1,6 @@
+export default {
+  info: 'border-primary-500 bg-primary-500/20 text-primary-900',
+  warning: 'border-yellow-600 bg-yellow-500/20 text-yellow-900',
+  error: 'border-pink-500 bg-pink-500/20 text-pink-900',
+  success: 'border-emerald-500 bg-emerald-500/20 text-emerald-900',
+}
