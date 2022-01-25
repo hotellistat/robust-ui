10e19f4ead53b70df62ab10b6cadc3ee0d386067
Daniel Schneider <daniel.schneider@eramux.com>
2022-01-25 11:10:03 +0100

    feat: add separator

---
+++ src/components/Separator/Separator.vue
@@ -0,0 +1,41 @@
+<template>
+  <div
+    v-if="!$slots.default"
+    class="flex-shrink-0"
+    :class="[colorClass, vert ? 'w-px self-stretch' : 'h-px w-full']"
+  ></div>
+  <div v-else class="flex items-center">
+    <div
+      :class="[
+        'flex-1 bg-gray-100 dark:bg-gray-700',
+        vert ? 'w-px self-stretch' : 'h-px w-full',
+      ]"
+    ></div>
+    <div class="flex-shrink-0 px-2 text-gray-300 dark:text-gray-500">
+      <slot />
+    </div>
+    <div
+      :class="[
+        'flex-1 bg-gray-100 dark:bg-gray-700',
+        vert ? 'w-px self-stretch' : 'h-px w-full',
+      ]"
+    ></div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  props: {
+    vert: {
+      type: Boolean,
+      default: false,
+    },
+    colorClass: {
+      type: String,
+      default: 'bg-gray-100 dark:bg-gray-700 ',
+    },
+  },
+})
+</script>
