893c48b3054ce0f5ed347c54b4f66ac89857e9c6
Daniel Schneider <daniel.schneider@eramux.com>
2022-01-28 18:07:23 +0100

    feat: change component export naming

--- src/components/ContentBox/ContentBox.vue
+++ src/components/ContentBox/ContentBox.vue
@@ -1,39 +1,43 @@
 <template>
-  <div
+  <section
     class="overflow-hidden rounded-md border border-gray-200 bg-white dark:border-gray-700 dark:bg-gray-800"
   >
-    <div
+    <header
       v-if="description !== undefined || title !== undefined"
       class="p-4 sm:p-6"
     >
-      <div class="flex">
+      <h1 class="flex">
         <div
           class="text-xl font-semibold"
           :class="[description !== undefined ? 'mb-2' : '']"
         >
           <slot name="title" :title="title">{{ title }}</slot>
         </div>
-      </div>
-      <div
+      </h1>
+      <p
         v-if="description !== undefined"
         class="text-gray-500 dark:text-gray-500"
       >
         <slot name="description" :description="description">{{
           description
         }}</slot>
-      </div>
-    </div>
-    <base-separator />
-    <section class="p-4 sm:p-6">
+      </p>
+    </header>
+    <Separator />
+    <div class="p-4 sm:p-6">
       <slot />
-    </section>
-  </div>
+    </div>
+  </section>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import Separator from '../Separator/Separator.vue'
 
 export default defineComponent({
+  components: {
+    Separator,
+  },
   props: {
     title: {
       type: String,
