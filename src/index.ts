d0146245d62abfef7d92508753add3bf5e6732c7
Daniel Schneider <daniel.schneider@eramux.com>
2022-02-13 12:49:17 +0100

    fix: default export

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,7 @@ export { lockScroll, unlockScroll } from './utils/scrollLock'
 
 import * as components from './components'
 
-export function install(app) {
+export default function install(app) {
   if (this.installed) return;
   this.installed = true;
   Object.entries(components).forEach(([componentName, component]) =>
