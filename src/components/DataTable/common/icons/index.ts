408763a2416acc4f168a0ea6b0fc54392cbb6e30
marekhotellistat <marek.gorski@hotellistat.de>
2022-02-28 19:52:29 +0100

    feat: remove icons

--- src/components/DataTable/common/icons/index.ts
+++
@@ -1,6 +0,0 @@
-export { default as SortAscIcon } from "./SortAscIcon.vue";
-export { default as SortDescIcon } from "./SortDescIcon.vue";
-export { default as MenuIcon } from "./MenuIcon.vue";
-export { default as PlusIcon } from "./PlusIcon.vue";
-export { default as MinusIcon } from "./MinusIcon.vue";
-
