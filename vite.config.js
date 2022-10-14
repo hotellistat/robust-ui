96e4904ebf1aaa71a532a94e916b393df0091e42
Daniel Schneider <daniel.schneider@eramux.com>
2022-10-14 08:59:57 +0200

    fix: base route

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { resolve } from 'path'
 import visualizer from 'rollup-plugin-visualizer'
 
 export default defineConfig(async (config) => ({
+  base: '/robust-ui/',
   plugins: [
     vue(),
     config.mode === 'analyze'
