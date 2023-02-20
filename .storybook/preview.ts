5867b5b669b79b668197bac4b10b939c3ae369a1
Daniel Schneider <daniel.schneider@hotellistat.de>
2023-02-20 16:43:59 +0100

    fix: minor issues

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,8 +1,5 @@
 import '../src/styles/tailwind.css'
 import '../src/styles/storybook.css'
-import '../src/types/global.d.ts';
-
-// import "../src/env"
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
