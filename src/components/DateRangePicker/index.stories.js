6077fdd00a1baad61e77e350de02b399f1578b44
Tarik Mehinagić <58049649+tarikmehinagic@users.noreply.github.com>
2023-03-14 13:17:12 +0100

    feat: calendar relative dates (#152)

--- src/components/DateRangePicker/index.stories.js
+++ src/components/DateRangePicker/index.stories.js
@@ -25,15 +25,46 @@ const Template = (args) => ({
 const date = [new Date(), new Date()];
 const dateRange = ref(date);
 const compareDateRange = ref(date);
+const activePreset = ref();
+const activeComparePreset = ref();
 
 export const Default = Template.bind({});
 Default.args = {
+  activePreset,
+  activeComparePreset,
   dateRange: dateRange,
   'onUpdate:dateRange': (val) => {
-    dateRange.value = val;
+    dateRange.value = val?.date;
+    if (val && val.type.name === 'preset') {
+      activePreset.value = val.type.value;
+    } else {
+      activePreset.value = undefined;
+    }
   },
   compareDateRange: compareDateRange,
   'onUpdate:compareDateRange': (val) => {
-    compareDateRange.value = val;
+    compareDateRange.value = val?.date;
+    if (val && val.type.name === 'preset') {
+      activeComparePreset.value = val.type.value;
+    } else {
+      activeComparePreset.value = undefined;
+    }
   },
+  'onUpdate:relative': (type) => {
+    if (type && type.name === 'preset') {
+      activePreset.value = type.value;
+    } else {
+      activePreset.value = undefined;
+    }
+  },
+  'onUpdate:compareRelative': (type) => {
+    if (type && type.name === 'preset') {
+      activeComparePreset.value = type.value;
+    } else {
+      activeComparePreset.value = undefined;
+    }
+  },
+  // 'onUpdate:relative': (val) => {
+  //   console.log('update:relative', val);
+  // },
 };
