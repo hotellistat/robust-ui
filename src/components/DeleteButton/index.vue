b9f981cf1cb5c97faaa1ab18d7c8e81624234209
Tarik Mehinagić <58049649+tarikmehinagic@users.noreply.github.com>
2023-03-21 08:06:57 +0100

    feat: delete modal (#158)
    * feat: delete modal
    
    * feat: delete modal

---
+++ src/components/DeleteButton/index.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <RobustModal v-model:opened="open" :size="size">
+      <template #title> Delete </template>
+      <div class="p-4">
+        <slot name="body" />
+        <div v-if="!$slots.body" class="flex flex-col gap-y-2">
+          <div class="mb-4">Are you sure you want to delete this item?</div>
+          <div class="flex justify-end">
+            <div class="flex gap-x-2">
+              <RobustButton variant="danger" class="uppercase" @click="confirm">
+                Delete</RobustButton
+              >
+              <RobustButton variant="muted" class="uppercase" @click="decline">
+                Cancel
+              </RobustButton>
+            </div>
+          </div>
+        </div>
+      </div>
+    </RobustModal>
+    <RobustButton v-bind="props" @click.stop="handleClick">
+      <slot />
+    </RobustButton>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { PropType, ref } from 'vue';
+import { RobustButton, RobustModal } from '..';
+
+const props = defineProps({
+  tag: {
+    type: String,
+    default: 'button',
+  },
+  loading: {
+    type: Boolean,
+    default: false,
+  },
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+  rounded: {
+    type: Boolean,
+    default: false,
+  },
+  variant: {
+    type: [String, Boolean] as PropType<string | false>,
+    default: 'danger',
+  },
+  condensed: {
+    type: Boolean,
+    default: false,
+  },
+  size: {
+    type: String,
+    default: () => 'sm',
+  },
+});
+
+const emit = defineEmits(['confirm', 'decline']);
+
+const confirm = () => {
+  open.value = false;
+  emit('confirm');
+};
+
+const decline = () => {
+  open.value = false;
+  emit('decline');
+};
+
+const open = ref(false);
+
+const handleClick = (e: MouseEvent) => {
+  if (e.shiftKey) return confirm();
+  open.value = true;
+};
+</script>
