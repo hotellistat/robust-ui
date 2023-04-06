c524f2813f4e7e0381e611f3d58f9024b1271201
dnlsndr <daniel.schneider@eramux.com>
2023-04-06 14:25:41 +0000

    Deploying to gh-pages from @ hotellistat/robust-ui@bf2f1b29747a6add32493ad165e67b89655335bf 🚀

--- assets/WithTooltip-5WWCYCAA-84833d25.js
+++
@@ -1,2 +0,0 @@
-import{b as h,b as l,d as s}from"./DocsRenderer-ED6OA6OB-ee79c9d2.js";import"./iframe-0cb5a4d9.js";import"../sb-preview/runtime.mjs";import"./_commonjsHelpers-725317a4.js";import"./index-d475d2ea.js";import"./index-a68517ca.js";import"./index-356e4a49.js";import"./preview-3493252b.js";export{h as WithToolTipState,l as WithTooltip,s as WithTooltipPure};
-//# sourceMappingURL=WithTooltip-5WWCYCAA-84833d25.js.map
---
+++ assets/index.stories-ca6bf977.js
@@ -0,0 +1,33 @@
+import{f as r}from"./index-853689a7.js";import{_ as n}from"./index-68f58b30.js";import"./vue.esm-bundler-7f60e394.js";import"./PhCaretRight.vue-ff6d2462.js";import"./index-d87ddea3.js";import"./PhCheck.vue-2562e881.js";import"./index-c94906d2.js";import"./index-0fb5aa92.js";import"./index-2ec087a1.js";import"./index-219ed869.js";import"./index-19484018.js";import"./index-1c9d7327.js";import"./index-9585fd4a.js";import"./index-22d9b209.js";import"./index-c1fbc973.js";import"./PhWarningCircle.vue-cbb6e471.js";import"./index-ef9e3050.js";import"./index-cdad79fe.js";import"./PhWarning.vue-7762b4f0.js";import"./index-88c349b4.js";import"./index-687cefe2.js";import"./index-b2c8899c.js";import"./index-66d9cb5d.js";import"./index-c665e89d.js";import"./index-09fde77f.js";import"./index-d307373b.js";import"./index-2936b9a0.js";import"./index-4ef99693.js";const j={title:"Ui/WidgetWrapper",component:r,argTypes:{title:{control:{type:"text"}},description:{control:{type:"text"}},draggable:{control:{type:"boolean"}}}},s=i=>({template:`<WidgetWrapper v-bind="args" class="test">
+    <template #title>Widget wrapper</template>
+    Test content
+    <template #options={close}>
+    <div style="padding: 16px">
+       Hello option
+       {{close}}
+       <RobustButton @click="close()" >Close</RobustButton>
+    </div>
+    </template>
+  </WidgetWrapper>`,components:{WidgetWrapper:r,RobustButton:n},setup(){return{args:i}}}),t=s.bind({});t.args={title:"WidgetWrapper title",description:"WidgetWrapper description text to be placed here"};var e,o,p;t.parameters={...t.parameters,docs:{...(e=t.parameters)==null?void 0:e.docs,source:{originalSource:`args => ({
+  template: \`<WidgetWrapper v-bind="args" class="test">
+    <template #title>Widget wrapper</template>
+    Test content
+    <template #options={close}>
+    <div style="padding: 16px">
+       Hello option
+       {{close}}
+       <RobustButton @click="close()" >Close</RobustButton>
+    </div>
+    </template>
+  </WidgetWrapper>\`,
+  components: {
+    WidgetWrapper,
+    RobustButton: Button
+  },
+  setup() {
+    return {
+      args
+    };
+  }
+})`,...(p=(o=t.parameters)==null?void 0:o.docs)==null?void 0:p.source}}};const q=["Default"];export{t as Default,q as __namedExportsOrder,j as default};
+//# sourceMappingURL=index.stories-ca6bf977.js.map
