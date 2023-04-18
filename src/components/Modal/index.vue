4b9dfbd67717bc991c77fbfdde075db68308a630
Daniel Schneider <daniel.schneider@eramux.com>
2023-04-18 19:46:27 +0200

    feat: improve colors

--- src/components/Modal/index.vue
+++ src/components/Modal/index.vue
@@ -25,7 +25,7 @@
           @keydown.esc="close"
         >
           <div
-            class="relative flex max-h-full min-h-0 w-full flex-col border border-gray-200 bg-white/20 backdrop-blur-2xl dark:border-gray-800 dark:bg-gray-600/20"
+            class="relative flex max-h-full min-h-0 w-full flex-col border border-gray-200 bg-white/40 backdrop-blur-3xl dark:border-gray-800 dark:bg-gray-800/40"
             :class="[
               modalClass,
               modalBoxInnerClass,
