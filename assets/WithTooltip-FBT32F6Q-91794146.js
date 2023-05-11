aeadb3236ff119bd2658de04987f2eebf1af0d2c
dnlsndr <daniel.schneider@eramux.com>
2023-05-11 06:13:50 +0000

    Deploying to gh-pages from @ hotellistat/robust-ui@0f33e65df4b40acdbf20554d381db904e26f2b5f 🚀

---
+++ assets/WithTooltip-FBT32F6Q-91794146.js
@@ -0,0 +1,2 @@
+import{b as e,b as h,d as l}from"./DocsRenderer-EYKKDMVH-923c5f94.js";import"./iframe-48c0e7fd.js";import"../sb-preview/runtime.js";import"./_commonjsHelpers-725317a4.js";import"./index-d475d2ea.js";import"./index-671c6938.js";import"./index-356e4a49.js";export{e as WithToolTipState,h as WithTooltip,l as WithTooltipPure};
+//# sourceMappingURL=WithTooltip-FBT32F6Q-91794146.js.map
---
+++ assets/index.stories-116cd330.js
@@ -0,0 +1,49 @@
+import{E as o}from"./index-6368aee8.js";import{I as a}from"./PhWarning.vue-12b26d3d.js";import{_ as b}from"./index-46ac9739.js";import{I as h}from"./index-de5d6c2f.js";import"./vue.esm-bundler-b4bdeeb0.js";import"./_plugin-vue_export-helper-c27b6911.js";import"./index-d11309f6.js";const x={title:"Ui/EmptyState",component:o,argTypes:{title:{control:{type:"text",default:"Title"}}}},y=t=>({template:'<EmptyState v-bind="args" />',components:{EmptyState:o},setup(){return{args:t}}}),e=y.bind();e.args={title:"Input title",description:"This is an input hint. It is supposed to help the user understand the reason this input exists and what it does",icon:a};const S=t=>({template:`<EmptyState v-bind="args" >
+  <RobustButton>Call to action</RobustButton>
+  </EmptyState>`,components:{EmptyState:o,RobustButton:b},setup(){return{args:t}}}),n=S.bind();n.args={title:"Input title",description:"This is an input hint. It is supposed to help the user understand the reason this input exists and what it does",icon:a};const E=t=>({template:`<EmptyState v-bind="args" >
+  <template #icon>
+    <RobustImg src="/assets/empty.svg" width="256"/>
+  </template>
+  <RobustButton>Call to action</RobustButton>
+  </EmptyState>`,components:{EmptyState:o,RobustButton:b,RobustImg:h},setup(){return{args:t}}}),s=E.bind();s.args={title:"Input title",description:"This is an input hint. It is supposed to help the user understand the reason this input exists and what it does",icon:a};var r,p,i;e.parameters={...e.parameters,docs:{...(r=e.parameters)==null?void 0:r.docs,source:{originalSource:`args => ({
+  template: \`<EmptyState v-bind="args" />\`,
+  components: {
+    EmptyState
+  },
+  setup() {
+    return {
+      args
+    };
+  }
+})`,...(i=(p=e.parameters)==null?void 0:p.docs)==null?void 0:i.source}}};var u,m,c;n.parameters={...n.parameters,docs:{...(u=n.parameters)==null?void 0:u.docs,source:{originalSource:`args => ({
+  template: \`<EmptyState v-bind="args" >
+  <RobustButton>Call to action</RobustButton>
+  </EmptyState>\`,
+  components: {
+    EmptyState,
+    RobustButton
+  },
+  setup() {
+    return {
+      args
+    };
+  }
+})`,...(c=(m=n.parameters)==null?void 0:m.docs)==null?void 0:c.source}}};var l,d,g;s.parameters={...s.parameters,docs:{...(l=s.parameters)==null?void 0:l.docs,source:{originalSource:`args => ({
+  template: \`<EmptyState v-bind="args" >
+  <template #icon>
+    <RobustImg src="/assets/empty.svg" width="256"/>
+  </template>
+  <RobustButton>Call to action</RobustButton>
+  </EmptyState>\`,
+  components: {
+    EmptyState,
+    RobustButton,
+    RobustImg
+  },
+  setup() {
+    return {
+      args
+    };
+  }
+})`,...(g=(d=s.parameters)==null?void 0:d.docs)==null?void 0:g.source}}};const _=["Default","CallToAction","CustomIcon"];export{n as CallToAction,s as CustomIcon,e as Default,_ as __namedExportsOrder,x as default};
+//# sourceMappingURL=index.stories-116cd330.js.map
