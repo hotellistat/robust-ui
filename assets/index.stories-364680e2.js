16355159708a8feeb7c3bae364d987a3232fda69
tarikmehinagic <58049649+tarikmehinagic@users.noreply.github.com>
2023-07-06 14:07:50 +0000

    Deploying to gh-pages from @ hotellistat/robust-ui@2ee734a8a3e375d28c5645719d5c094b471ba266 🚀

---
+++ assets/index.stories-364680e2.js
@@ -0,0 +1,12 @@
+import{_ as s}from"./index-4ceae020.js";import"./vue.esm-bundler-c29c03ae.js";import"./PhWarning.vue-045f1f2d.js";const m={title:"Ui/Notice",component:s,argTypes:{title:{control:{type:"text",default:"Title"}}}},a=r=>({template:'<Notice v-bind="args" >This is a notice</Notice>',components:{Notice:s},setup(){return{args:r}}}),e=a.bind();var t,o,n;e.parameters={...e.parameters,docs:{...(t=e.parameters)==null?void 0:t.docs,source:{originalSource:`args => ({
+  template: \`<Notice v-bind="args" >This is a notice</Notice>\`,
+  components: {
+    Notice
+  },
+  setup() {
+    return {
+      args
+    };
+  }
+})`,...(n=(o=e.parameters)==null?void 0:o.docs)==null?void 0:n.source}}};const l=["Default"];export{e as Default,l as __namedExportsOrder,m as default};
+//# sourceMappingURL=index.stories-364680e2.js.map
