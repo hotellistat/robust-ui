9953d29d8082f6f3664c4fe4389105fb6595a310
dnlsndr <daniel.schneider@eramux.com>
2023-09-03 13:05:19 +0000

    Deploying to gh-pages from @ hotellistat/robust-ui@c70aaf49e1bf458376dee2a72bbf766f4fa29ee4 🚀

---
+++ assets/index.stories-dfd91169.js
@@ -0,0 +1,14 @@
+import{r as i}from"./vue.esm-bundler-4b093791.js";import{_ as o}from"./index-28ca9316.js";import"./floating-ui.dom-28dd0c9f.js";const d={title:"Ui/FLoating",component:o,argTypes:{title:{control:{type:"text"}},description:{control:{type:"text"}}}},l=r=>({template:'<FLoating v-bind="args" v-model="value">ldsajfk</FLoating>',components:{FLoating:o},setup(){const s=i(!1);return{args:r,value:s}}}),t=l.bind({});t.args={title:"FLoating title",description:"FLoating description text to be placed here"};var e,n,a;t.parameters={...t.parameters,docs:{...(e=t.parameters)==null?void 0:e.docs,source:{originalSource:`args => ({
+  template: '<FLoating v-bind="args" v-model="value">ldsajfk</FLoating>',
+  components: {
+    FLoating
+  },
+  setup() {
+    const value = ref(false);
+    return {
+      args,
+      value
+    };
+  }
+})`,...(a=(n=t.parameters)==null?void 0:n.docs)==null?void 0:a.source}}};const g=["Default"];export{t as Default,g as __namedExportsOrder,d as default};
+//# sourceMappingURL=index.stories-dfd91169.js.map
