f338c25169ba68c48b4efa479b30905c1a474cba
dnlsndr <daniel.schneider@eramux.com>
2023-09-28 18:29:15 +0000

    Deploying to gh-pages from @ hotellistat/robust-ui@e364ed371ef3c79d30506c08790fa937aa0ed84e 🚀

---
+++ assets/index.stories-076f6b8a.js
@@ -0,0 +1,18 @@
+import{d as i}from"./index-63b54f4f.js";import"./vue.esm-bundler-22b53bfc.js";import"./index-bdfed0c8.js";import"./PhCheck.vue-4eaa9e07.js";import"./index-0209670b.js";import"./floating-ui.dom-28dd0c9f.js";import"./PhCaretRight.vue-ab999ed8.js";import"./index-3c486f74.js";import"./index-f8a39b50.js";import"./index-21a0a241.js";import"./index-806109a3.js";import"./index-70413851.js";import"./_plugin-vue_export-helper-c27b6911.js";import"./index-71bc0e30.js";import"./index-09ffe11c.js";import"./index-f0e16eec.js";import"./index-3e7c15ad.js";import"./PhCaretDown.vue-8f79e38b.js";import"./index-ea618137.js";import"./PhWarningCircle.vue-46d5e3f6.js";import"./index-77b5f578.js";import"./index-c9ac3d5c.js";import"./index-65fa0a4f.js";import"./PhWarning.vue-967f20ed.js";import"./index-c0a29e75.js";import"./index-2b207480.js";import"./index-af568efc.js";import"./index-956cca8d.js";import"./index-104bc87b.js";import"./index-da374c1b.js";import"./index-8f2a5f12.js";import"./_commonjsHelpers-de833af9.js";const G={title:"Ui/ConfirmationModal",component:i,argTypes:{"onUpdated:opened":{action:"ConfirmationModal"},onConfirm:{action:"confirm"},onCancel:{action:"cancel"},opened:{control:{type:"boolean"}},size:{options:["sm","md","lg","xl","2xl","3xl","full"],control:{type:"select"}}},args:{opened:!0}},o={render:e=>({components:{ConfirmationModal:i},setup:()=>({args:e}),template:`
+<ConfirmationModal v-slot="{ click }" v-model:opened="args.opened" v-bind="args">
+  <button @click="click">Open</button>
+</ConfirmationModal>`})};var t,r,n;o.parameters={...o.parameters,docs:{...(t=o.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  render: args => ({
+    components: {
+      ConfirmationModal
+    },
+    setup: () => ({
+      args
+    }),
+    template: \`
+<ConfirmationModal v-slot="{ click }" v-model:opened="args.opened" v-bind="args">
+  <button @click="click">Open</button>
+</ConfirmationModal>\`
+  })
+}`,...(n=(r=o.parameters)==null?void 0:r.docs)==null?void 0:n.source}}};const H=["Default"];export{o as Default,H as __namedExportsOrder,G as default};
+//# sourceMappingURL=index.stories-076f6b8a.js.map
