d7a1bdc5d68aa34ce80625e3628f6f82fcec048f
dnlsndr <daniel.schneider@eramux.com>
2024-09-27 12:17:49 +0000

    Deploying to gh-pages from @ hotellistat/robust-ui@d932223c0cad996edf527537551bf71c55051e09 🚀

---
+++ assets/index.stories-7941a083.js
@@ -0,0 +1,25 @@
+import{_ as a}from"./index-7dab12d0.js";import{r as m}from"./vue.esm-bundler-5fdd8ab6.js";import"./PhCheck.vue-f0a19b25.js";const h={title:"Ui/Checkbox",component:a,argTypes:{title:{control:{type:"text"}},description:{control:{type:"text"}}}},i=o=>({template:'<Checkbox v-bind="args" v-model="value"></Checkbox>',components:{Checkbox:a},setup(){return{value:m(!1),args:o}}}),e=i.bind({});e.args={title:"Checkbox title"};const d=o=>({template:'<Checkbox v-model="value"><template v-slot:title>{{args.title}}</template></Checkbox>',components:{Checkbox:a},setup(){return{value:m(!1),args:o}}}),t=d.bind({});t.args={title:"V-Slot Checkbox title"};var s,n,r;e.parameters={...e.parameters,docs:{...(s=e.parameters)==null?void 0:s.docs,source:{originalSource:`args => ({
+  template: '<Checkbox v-bind="args" v-model="value"></Checkbox>',
+  components: {
+    Checkbox
+  },
+  setup() {
+    const value = ref(false);
+    return {
+      value,
+      args
+    };
+  }
+})`,...(r=(n=e.parameters)==null?void 0:n.docs)==null?void 0:r.source}}};var l,c,p;t.parameters={...t.parameters,docs:{...(l=t.parameters)==null?void 0:l.docs,source:{originalSource:`args => ({
+  template: '<Checkbox v-model="value"><template v-slot:title>{{args.title}}</template></Checkbox>',
+  components: {
+    Checkbox
+  },
+  setup() {
+    const value = ref(false);
+    return {
+      value,
+      args
+    };
+  }
+})`,...(p=(c=t.parameters)==null?void 0:c.docs)==null?void 0:p.source}}};const k=["Default","SlottedTitle"];export{e as Default,t as SlottedTitle,k as __namedExportsOrder,h as default};
